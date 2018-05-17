7abba31f625d2a93495ee0b96e3e2e8d6f6f7f0f
Kostas Nikolopoulos <knikolopoulos@vivantehealth.com>
2018-05-17 17:36:05 +0300

    Added UUIDv4Validator

---
+++ src/UUIDv4Validator.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Phramework\Validate;
+
+/**
+ * @author Spafaridis Xenofon <nohponex@gmail.com>
+ * @author Nikolopoulos Konstantinos <kosnikolopoulos@gmail.com>
+ */
+class UUIDv4Validator extends StringValidator
+{
+    /**
+     * Overwrite base class type
+     * @var string
+     */
+    protected static $type = 'UUIDv4';
+
+    public function __construct()
+    {
+        $pattern = '/^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/';
+
+        parent::__construct(0, null, $pattern, null);
+    }
+}
